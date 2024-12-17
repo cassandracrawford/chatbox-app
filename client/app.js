79c4602bce19d0d4c6f4124d3a33911cb9981174
Cassandra Crawford <crfeliciano1989@gmail.com>
2024-12-16 19:31:32 -0700

    Enable users to send messages with initials

---
+++ client/app.js
@@ -0,0 +1,21 @@
+var socket = io(); // socket is now a reference to the Socket.IO library
+
+// $ function - use of JQuery
+// on: a function that accepts another function as parameter
+$('form').on('submit', function () {
+    var text = $('#initials').val() + ' says: ' + $('#message').val();
+    socket.emit('message', text);
+    $('#initials').val('');
+    $('#message').val('');
+    return false;
+});
+
+socket.on('message', function (messages) {
+     // Clear the #history list to avoid duplicates
+     $('#history').empty();
+
+    // Loop through the array and append each message to the list
+    messages.forEach(function (msg) {
+        $('<li>').text(msg).appendTo('#history');
+    });
+});
---
+++ server.js
@@ -0,0 +1,30 @@
+// load external modules
+var express = require('express'); // imports the Express.js framework for server creation in Node.js
+var app = express(); //express application instance to handle routes and middleware
+
+var http = require('http'); // imports the built-in http module to create an HTTP server
+var server = http.Server(app); // creates an HTTP server and ties it to Express app
+
+// middleware serves static files located in the client folder 
+// can be accessed directly in the browser
+app.use(express.static('client')); 
+
+// use of server.io to enable bidirectional communication
+// initializes Socket.IO
+var io = require('socket.io')(server); // (server) passesthe HTTP server instance to Socket.IO to listen for WebSocket connections
+const list_msg = [];
+
+io.on('connection', function (socket){
+    // Send the existing messages to the new client
+    socket.emit('message', list_msg);
+
+    socket.on('message', function (msg) {
+        list_msg.push(msg);
+        io.emit('message', list_msg); // broadcasts the message to all connected clients, including the sender
+    });
+});
+
+// starts server on port 8080 (commonly used for local development)
+server.listen(8080, function() {
+    console.log('Chat server running');
+});
